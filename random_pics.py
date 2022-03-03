57c24a9a2c697dec140db7eb8ea0f55cb72a4289
Prateek Mittal <prateekmittal154@gmail.com>
2022-03-03 08:57:29 +0530

    Downloading random stock images
    This program aims to download a random number of images into the current working directory.

--- random_pics.py
+++ random_pics.py
@@ -3,15 +3,11 @@ import os
 import urllib.request
 
 
-# Welcome message
-print('Download random stock images from picsum.photos\n(* images download to the current directory)')
-
 # Paths
 # get current working directory
 cwd = os.getcwd()
 
-# Images
-# python 2 uses raw_input() to return a string, python 3 uses input()
+
 imgNum = input('Enter number of images: ')
 imgDim = input('Enter image size (e.g. 1920x1080): ').split('x')
 
