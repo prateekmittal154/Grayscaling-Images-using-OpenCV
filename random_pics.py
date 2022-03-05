e543f880f7643488698545a6de4cc75dab6ff45e
Prateek Mittal <prateekmittal154@gmail.com>
2022-03-05 21:38:59 +0530

    Update random_pics.py

--- random_pics.py
+++ random_pics.py
@@ -1,18 +1,16 @@
-# Imports
+
 import os
 import urllib.request
 
 
-# Paths
-# get current working directory
+
 cwd = os.getcwd()
 
 
 imgNum = input('Enter number of images: ')
 imgDim = input('Enter image size (e.g. 1920x1080): ').split('x')
 
-# change directory to current working directory
-# os.path.join(a,b) if you want to allow custom paths
+
 os.chdir(cwd)
 
 
